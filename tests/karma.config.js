2b7ffdd1520e40202ea3578d09cc119da3739cc6
Vladimir Mollov <vmollog@gmail.com>
2015-02-17 16:50:48 -0500

    fix to karma config

--- tests/karma.config.js
+++ tests/karma.config.js
@@ -16,9 +16,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'bower_components/jquery/dist/jquery.js',
+        'bower_components/jquery/dist/jquery.min.js',
         'bower_components/angular/angular.min.js',
         'bower_components/angular-mocks/angular-mocks.js',
+        'bower_components/angular-*/*.js',
         'app/app.js',
         'app/**/*.js',
         'app/directives/*.html',
@@ -29,6 +30,7 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
+        'app/bower/**/*'
         /*'tests/unit/controllers*//*.js',
         'tests/unit/services*//*.js'*/
     ],
