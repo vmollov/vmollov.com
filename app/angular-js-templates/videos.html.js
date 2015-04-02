520658bed18d9cec278be6d1b737a906ecf0aad1
Vladimir Mollov <vmollog@gmail.com>
2015-04-01 23:20:08 -0400

    tweaks

--- app/angular-js-templates/videos.html.js
+++ app/angular-js-templates/videos.html.js
@@ -1,4 +1,4 @@
 angular.module('vmMusic').run(['$templateCache', function($templateCache) {
     $templateCache.put('/components/videos.html',
-        "<div class=\"video itemBackground\" data-ng-repeat=\"video in allVideos\">\n\t<video-sample video-src=\"video.embedUrl\" video-title=\"video.title\" video-thumbnail=\"video.thumbnail\"></video-sample>\n    <div class=\"videoDetails\">\n        <div class=\"itemTitle\">{{ video.title }}</div>\n        <div class=\"info\">{{ video.description }}</div>\n    </div>\n</div>\n");
+        "<h1>YouTube Feed</h1>\n<div class=\"video itemBackground\" data-ng-repeat=\"video in allVideos\">\n\t<video-sample video-src=\"video.embedUrl\" video-title=\"video.title\" video-thumbnail=\"video.thumbnail\"></video-sample>\n    <div class=\"videoDetails\">\n        <div class=\"itemTitle\">{{ video.title }}</div>\n        <div class=\"info\">{{ video.description }}</div>\n    </div>\n</div>\n");
 }]);
