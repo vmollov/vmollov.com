05389fc6666d68337e46fc16d9d57faaaf001351
Vladimir Mollov <vmollov@gmail.com>
2017-07-21 08:44:31 -0400

    bugfix

--- app/components/bioCtrl.js
+++ app/components/bioCtrl.js
@@ -1,12 +1,14 @@
 angular.module('vmMusic').controller('bioCtrl', [
     '$scope',
+    '$sce',
     '$http',
-    function($scope, $http){
+    function($scope, $sce, $http){
         'use strict';
 
         $http.get('/data/bio.json').then(
             function(response){
                 $scope.bio = response.data;
+                $scope.bio.biography = $sce.trustAsHtml(response.data.biography);
             },
             function(error){
                 console.log('An error occurred while getting biography data. ' + error);
