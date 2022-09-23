921a8bfa177656a904cc61ce861784fe4b409b1d
Vladimir Mollov <vmollov@gmail.com>
2022-09-23 16:10:18 -0400

    Correcting a typo

--- app/angular-js-templates/privacy.html.js
+++ app/angular-js-templates/privacy.html.js
@@ -1,4 +1,4 @@
 angular.module('vmMusic').run(['$templateCache', function($templateCache) {
     $templateCache.put('/components/privacy.html',
-        "<div class=\"privacyContainer\">\n    <p>\n        This website uses only publicly available data from the the YouTube API which is governed by the YouTube <a href=\"https://www.youtube.com/t/terms\" target=\"_blank\">Terms of Service</a>.\n    </p>\n</div>");
+        "<div class=\"privacyContainer\">\n    <p>\n        This website uses only publicly available data from the YouTube API which is governed by the YouTube <a href=\"https://www.youtube.com/t/terms\" target=\"_blank\">Terms of Service</a>.\n    </p>\n</div>\n");
 }]);
